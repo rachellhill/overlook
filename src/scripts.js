d1916a48767c46c93afe8f9bee8d5f22ba4fc03c
Rachel Hill <hillrachel187@gmail.com>
2022-04-24 21:35:47 -0600

    Create filter functionality

--- src/scripts.js
+++ src/scripts.js
@@ -10,6 +10,9 @@ let customerBookings = document.querySelector('.customer-reservations-container'
 let totalCost = document.querySelector('.total-cost-container');
 let dateInput = document.querySelector(".date-input");
 let showAvailableRoomsBtn = document.querySelector(".show-rooms-btn");
+let showAvailableRooms = document.querySelector(".show-available-rooms-section");
+let filterRoomsBtn = document.querySelector('.room-filter');
+let filterOptions = document.querySelector('.room-type-selection');
 
 
 // ----------------- GLOBAL VARIABLES ----------------- //
@@ -19,6 +22,7 @@ let bookingsData = [];
 let roomsData = [];
 let currentCustomer;
 let selectedDate;
+let availableRooms;
 
 // ----------------- functions ----------------- //
 const getApiData = () => {
@@ -43,37 +47,6 @@ const getApiData = () => {
   });
 };
 
-// const showData = () => {
-//   console.log("outside customers", customersData)
-//   console.log("outside bookings", bookingsData)
-//   console.log("outside rooms", roomsData)
-// }
-
-// showAvailable rooms
-// get the date from query selector -> date input
-// ensure the min of today's date -> use get date and set today as min in HTML element
-// replaceAll('-', '/') -> dates are diff
-
-//check booking date - iterate over bookingsData to check if date input !== booking.date -> filter
-
-// reduce over filtered array and forEach over roomData to check if booking.room === room.numer && filtered array does not already include that room, then push to acc
-
-// if dates match, room is not available
-  // if room numbers match, remove from acc (pop?)
-
-const findAvailableRooms = (bookingsData, roomsData) => {
-  selectedDate = dateInput.value.replaceAll('-', '/')
-
-  const bookedRooms = bookingsData.filter((booking) => {
-    return booking.date === selectedDate
-  }).map(booking => booking.roomNumber)
-  // console.log(bookedRooms)
-
-  const availableRooms = roomsData.filter(room => (!bookedRooms.includes(room.number)))
-  console.log(availableRooms)
-}
-
-
 const renderBookings = () => {
   currentCustomer.getCustomerBookings(bookingsData)
   currentCustomer.getAllRooms(roomsData)
@@ -93,14 +66,14 @@ const renderBookings = () => {
     <p class="room-cost">Cost per night: ${total}</p>
     </section>
     `
-  })
+  });
   console.log(currentCustomer.sortedBookings.length)
-}
+};
 
 const currencyFormatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD'
-})
+});
 
 const renderTotalCost = () => {
   currentCustomer.getCustomerBookings(bookingsData)
@@ -109,7 +82,7 @@ const renderTotalCost = () => {
   const totalDisplay = currencyFormatter.format(getCost)
   totalCost.innerHTML += `
     <h5 class="total-spend">${totalDisplay}</h5>`
-}
+};
 // need to access roomsBooked in the customer class and iterate over each to create an HTML element for customerBookings
 // function - find bidet
 
@@ -118,15 +91,55 @@ const instantiateCustomer = (id) => {
   customersData.forEach(customer => {
     if (customer.id === id) {
       customerArg = customer
-    }
-  })
+    };
+  });
   currentCustomer = new Customer(customerArg)
   return currentCustomer;
 };
-// eventually when api is incorporated, this will move into the .then
+
+const findAvailableRooms = (bookingsData, roomsData) => {
+  selectedDate = dateInput.value.replaceAll('-', '/')
+  const bookedRooms = bookingsData.filter((booking) => {
+    return booking.date === selectedDate
+  }).map(booking => booking.roomNumber)
+  console.log(bookedRooms)
+  availableRooms = roomsData.filter(room => (!bookedRooms.includes(room.number)))
+  console.log(availableRooms)
+
+  availableRooms.forEach(availableRoom => {
+    showAvailableRooms.innerHTML += `
+    <button class="available-room" id="${availableRoom.number}">
+      <h5>${availableRoom.roomType}</h5>
+      <p>Bed Size: ${availableRoom.bedSize}</p>
+      <p>Bed Size: ${availableRoom.costPerNight}</p>
+    </button>
+    `
+  })
+};
+
+// if an option is selected, filter available rooms to show only one of those options?
+
+// compare the joined option value to the classname and then if true, filter the array
+// const renderFilteredData = () => {
+//   let filter = event.target.className
+
+  // e.target.classname
+  // new array of smusheded roomTypes
+  // forEach over available rooms roomTypes
+    // if e.target.classname === to room.roomtype.join('')
+      // push into a new array
+  // iterate over available rooms and return room.type.includes(joined string)
+// }
+
+const showElement = (element) => {
+  element.classList.remove('hidden');
+};
+
+const hideElement = (element) => {
+  element.classList.add('hidden');
+};
 
 setTimeout(() => {console.log(currentCustomer)}, 5000);
-
 // will change on login iteration
   // match up id from login to the id in customersData and use that to set new customer object which will instantiate our current customer.
   // parseint to a number
@@ -136,8 +149,36 @@ setTimeout(() => {console.log(currentCustomer)}, 5000);
 
 window.onload = (event) => getApiData();
 showAvailableRoomsBtn.addEventListener("click", (e) => {
-  // console.log(bookingsData)
   findAvailableRooms(bookingsData, roomsData);
-  console.log("rooms data length", roomsData.length)
+  showElement(filterRoomsBtn);
+  hideElement(showAvailableRoomsBtn);
+  // console.log("rooms data length", roomsData.length)
   event.preventDefault();
-})
+});
+
+filterOptions.addEventListener("change", (e) => {
+  let filterSelected = filterOptions.value;
+  let availableRoomsByFilter = [];
+  availableRooms.forEach(availableRoom => {
+    if (availableRoom.roomType === filterSelected) {
+      availableRoomsByFilter.push(availableRoom)
+    };
+  });
+  showAvailableRooms.innerHTML = '';
+  availableRoomsByFilter.forEach(room => {
+    showAvailableRooms.innerHTML += `
+      <button class="available-room" id="${room.number}">
+        <h5>${room.roomType}</h5>
+        <p>Bed Size: ${room.bedSize}</p>
+        <p>Number of Beds: ${room.numBeds}</p>
+        <p>Cost: ${room.costPerNight}</p>
+      </button>
+    `
+  })
+// REFACTOR: make a function and pass in e as param
+  // invoke function with e in event listener ln 168 - 175 in another function
+  console.log(availableRoomsByFilter)
+});
+
+// click on room - give them the same classList
+  // all injected room buttons have the same className
