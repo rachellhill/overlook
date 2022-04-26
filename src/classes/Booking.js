c885a066bb90026210b677922ef129985ec6d847
Rachel Hill <hillrachel187@gmail.com>
2022-04-26 17:09:03 -0600

    Clean up code

--- src/classes/Booking.js
+++ src/classes/Booking.js
@@ -4,14 +4,8 @@ class Booking {
     this.userID = bookingData.userID;
     this.date = bookingData.date;
     this.roomNumber = bookingData.roomNumber;
-  }
-}
+  };
+};
 
 
-export default Booking
-
-// I should be able to select a date for which I’d like to book a room for myself
-// Upon selecting a date, I should be shown a list of room details for only rooms that are available on that date
-// I should be able to filter the list of available rooms by their roomType property
-// I should be able to select a room for booking
-// In the event that no rooms are available for the date/roomType selected, display a message fiercely apologizing to the user and asking them to adjust their room search
+export default Booking;
